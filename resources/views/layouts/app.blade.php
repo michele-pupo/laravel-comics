bb6ffde1196c62f4a50905afe680a78d85452343
michele-pupo <mikypupo@hotmail.it>
2024-04-09 16:33:09 +0200

    recupero repo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
 
     @include('partials/blueBundles')
 
+    @include('partials/footerLinks')
+
     @yield('content')
 
 </body>
---
+++ resources/views/partials/footerLinks.blade.php
@@ -0,0 +1,23 @@
+<div id="footer-links">
+    <div class="container">
+        <div id="bg-image">
+            <div id="links-image">
+                <div id="links-list-title">
+                    <h5>Dc comics</h5>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div id="image-logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
