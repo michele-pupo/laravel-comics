44f213aa4b3dab7c48837d9258ff76d36bef50a8
michele-pupo <mikypupo@hotmail.it>
2024-04-09 12:46:10 +0200

    inserted movies dynamically

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,28 +1,10 @@
 <div id="main">
     <div class="container row row-cols-6" id="movie-list">
+        @foreach($movie as $movieItem)
         <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
+            <img class="movie-img" src="{{ $movieItem['thumb'] }}" alt="">
+            <h3 class="movie-title">{{ $movieItem['title'] }}</h3>
         </div>
-        <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
-        </div>
-        <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
-        </div>
-        <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
-        </div>
-        <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
-        </div>
-        <div class="movie">
-            <img class="movie-img" src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-            <h3 class="movie-title">Action comics</h3>
-        </div>
+        @endforeach
     </div>
 </div>
